ff6fa041601cb2076eb3219fab5650a0ed55d055
SebaListello <sebastianlistello98@gmail.com>
2022-11-01 18:54:17 -0300

    Hello json

--- myApp.js
+++ myApp.js
@@ -4,9 +4,16 @@ let app = express();
 console.log("Hello World")
 
 app.use("/public", express.static(__dirname + "/public"));
+
 app.get('/', (req, res) => {
 //   res.send('Hello Express')
   res.sendFile(__dirname + "/views/index.html");
+})
+
+app.get('/json', (req, res) => {
+  res.json({
+    message: "Hello json"
+  })
 })
 
 
