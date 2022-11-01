29035418bb011b397c89be8ef86ede2b06127429
SebaListello <sebastianlistello98@gmail.com>
2022-11-01 18:51:18 -0300

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -3,6 +3,7 @@ let app = express();
 
 console.log("Hello World")
 
+app.use("/public", express.static(__dirname + "/public"));
 app.get('/', (req, res) => {
 //   res.send('Hello Express')
   res.sendFile(__dirname + "/views/index.html");
