b425d2b191861c8a1206ae0f7e0c49a032c20f5e
SebaListello <sebastianlistello98@gmail.com>
2022-11-03 19:30:28 -0300

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -3,6 +3,7 @@ const express = require('express');
 const app = express();
 const bodyParser = require("body-parser");
 
+app.use(bodyParser.urlencoded({ extended: false }));
 // console.log("Hello World")
 
 // app.use("/public", express.static(__dirname + "/public"));
